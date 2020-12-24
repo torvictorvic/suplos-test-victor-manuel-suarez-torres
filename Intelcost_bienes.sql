bbc50576fa40c3d7aa8628ee34eda0ddc5658332
victor <victormst@gmail.com>
2020-12-23 23:05:53 -0500

    Test - Vaciado de tabla

--- Intelcost_bienes.sql
+++ Intelcost_bienes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 23-12-2020 a las 22:53:05
+-- Tiempo de generación: 23-12-2020 a las 23:05:17
 -- Versión del servidor: 5.7.32-0ubuntu0.16.04.1
 -- Versión de PHP: 7.2.34-2+ubuntu16.04.1+deb.sury.org+1
 
@@ -41,18 +41,6 @@ CREATE TABLE `property` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
--- Volcado de datos para la tabla `property`
---
-
-INSERT INTO `property` (`id`, `id_json`, `address`, `city`, `telephone`, `postal_code`, `type`, `price`, `valid`) VALUES
-(1, 1, 'Ap #549-7395 Ut Rd.', 'New York', '334-052-0954', '85328', 'Casa', '$30,746', 1),
-(2, 2, 'P.O. Box 657, 9831 Cursus St.', 'Orlando', '488-441-5521', '04436', 'Casa de Campo', '$71,045', 1),
-(3, 61, 'Ap #214-5963 Metus Road', 'Houston', '337-930-6310', '5290KA', 'Casa', '$71,048', 1),
-(4, 30, '4406 Justo Avenue', 'Houston', '242-441-7733', '38707', 'Casa de Campo', '$69,433', 1),
-(5, 4, '347-866 Laoreet Road', 'Los Angeles', '997-640-8188', '94526-134', 'Casa de Campo', '$16,048', 1),
-(6, 48, 'P.O. Box 264, 6488 Euismod Avenue', 'Los Angeles', '210-220-4305', 'J6H 9S9', 'Apartamento', '$33,141', 1);
-
---
 -- Índices para tablas volcadas
 --
 
@@ -70,7 +58,7 @@ ALTER TABLE `property`
 -- AUTO_INCREMENT de la tabla `property`
 --
 ALTER TABLE `property`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
